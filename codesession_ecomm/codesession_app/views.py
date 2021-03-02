695a42ed6ba2dd5942fb50a781d44f7fe9dbf506
coder-sanju <sanju2help@gmail.com>
2021-03-02 18:29:21 +0530

    exclude filter

--- codesession_ecomm/codesession_app/views.py
+++ codesession_ecomm/codesession_app/views.py
@@ -11,11 +11,15 @@ import datetime
 
 @login_required(login_url='login')
 def Dashboard(request):
-    # AllProduct = Product.objects.all()
+    #varaible_name =  ModelName.objects.filter(**parameters)
+    AllProduct = Product.objects.filter(Product_Type=1)
+
+    # AllProduct = Product.objects.exclude(
+    #     Product_create__year=2021)
     # AllProduct = Product.objects.filter(model_field_name="value")
-    AllProduct = Product.objects.filter(
-        Product_create__lte=datetime.date.today())
-    print("linr number 18", datetime.date.today())
+    # AllProduct = Product.objects.exclude(
+    #     Product_create__lte=datetime.datetime.today())
+    print("linr number 18", datetime.datetime.today() - datetime.timedelta(days=1))
     print(AllProduct.query)
     return render(request, "Admin_html/main.htm", {'AllProduct': AllProduct})
 
